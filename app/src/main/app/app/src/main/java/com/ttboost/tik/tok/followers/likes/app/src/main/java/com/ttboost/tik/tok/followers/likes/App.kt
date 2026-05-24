d1963eeb579e0f81034626a579a88e6a91e791d2
lecongdzz <congle4443@gmail.com>
2026-05-24 18:14:00 +0700

    Add App class for application initialization

---
+++ app/src/main/app/app/src/main/java/com/ttboost/tik/tok/followers/likes/app/src/main/java/com/ttboost/tik/tok/followers/likes/App.kt
@@ -0,0 +1,10 @@
+package com.ttboost.tik.tok.followers.likes
+
+import android.app.Application
+
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        SecurityUtils.performSecurityChecks(this)
+    }
+}
